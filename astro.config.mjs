3a6c8c7b4c758a27b91c961bfba296244303e53d
Everthon de Souza Oliveira <everthonsol@yahoo.com.br>
2024-12-17 00:33:42 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://everthonso.github.io/', // Coloque o link do seu repositório GitHub Pages
+  base: '/everthonso.github.io/', // Caminho base do repositório
+});
